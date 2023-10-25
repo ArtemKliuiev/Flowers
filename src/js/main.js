10bf96b621f68daa6c0765107224a85eb95532bc
ArtemKliuiev <117043590+ArtemKliuiev@users.noreply.github.com>
2023-10-25 10:05:10 +0300

    start project

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import Test from './modules/test';
+
+
+
+
+
---
+++ src/js/modules/test.js
@@ -0,0 +1,11 @@
+export default class Test{
+    constructor(){
+        this.consoleLog()
+    }
+    consoleLog(){
+        console.log('метод класса "Test"');
+    }
+    methodTwo(){
+        console.log('ВТОРОЙ метод класса "Test"');
+    }
+}
