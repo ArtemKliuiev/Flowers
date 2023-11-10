6d62a5b7bc5f721e2f849aa89d453ea6b7b77abc
artem <mr_artem>
2023-11-10 12:54:56 +0200

    remove filter and dropdown

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -1,38 +1,84 @@
-
+import Dropdown from './modules/dropdown';
 class Catalog{
     constructor(){
         this.menuLeft = document.querySelector('.left-menu');
         this.itemsFiler = document.querySelectorAll('.left-menu__menu-item');
         this.itemsSortAdaptive = document.querySelectorAll('.left-menu__adaptive-sort-item');
-        this.tabs();
+        this.leftMenuBtn = document.querySelector('.button-for-left-menu');
+        this.deletFilterBtn = document.querySelector('.left-menu__adaptive-delete-filters');
+        this.exitApativeLeftMenu = document.querySelector('.left-menu__adaptive-back');
+        this.dropdown = new Dropdown('.drop-1');
+        this.adaptiveFilter()
+        this.tabsFilter();
         this.tabsSortAdaptive();
+        this.deletFilter();
     }
-    removeActive(el){
-        document.querySelectorAll('.left-menu__menu-item-title-active').forEach(item => {
-            const title = item.querySelector('.left-menu__menu-item-title');
-            if(el !== title){
-                item.classList.remove('left-menu__menu-item-title-active');
-            }               
+    adaptiveFilter(){
+        console.log(this.leftMenuBtn)
+        this.leftMenuBtn.addEventListener('click', () => {
+            this.menuLeft.classList.add('left-menu_active');
+            document.body.style.overflow = 'hidden';
         });
-    };
-    tabs(){
+        this.exitApativeLeftMenu.addEventListener('click', () => {
+            this.menuLeft.classList.remove('left-menu_active');
+
+            document.body.style.overflow = 'auto';
+        });
+    }
+    tabsFilter(){
+        function removeActive(el){
+            document.querySelectorAll('.left-menu__menu-item-title-active').forEach(item => {
+                const title = item.querySelector('.left-menu__menu-item-title');
+                if(el !== title){
+                    item.classList.remove('left-menu__menu-item-title-active');
+                }    
+            });
+        };
         this.itemsFiler.forEach(item => {
             const title = item.querySelector('.left-menu__menu-item-title');
             title.addEventListener('click', (e) => {
-                this.removeActive(e.target)
-                item.classList.toggle('left-menu__menu-item-title-active')
+                removeActive(e.target);
+                item.classList.toggle('left-menu__menu-item-title-active');
             })
         })
     }
     tabsSortAdaptive(){
-        this.itemsSortAdaptive.forEach(item => {
-            item.addEventListener('click', () => {
-                document.querySelectorAll('.left-menu__adaptive-sort-item').forEach(item => {
-                    item.classList.remove('left-menu__adaptive-sort-item_active');     
+        const menuLeft = this.menuLeft;
+        menuLeft.addEventListener('click', (e) => {
+            if(e.target.classList[0] === 'left-menu__adaptive-back'){
+                menuLeft.classList.remove('left-menu_sort-active');
+                this.itemsSortAdaptive.forEach(item => {
+                    item.classList.remove('left-menu__adaptive-sort-item_active')
                 });
-                this.menuLeft.classList.add('left-menu_sort-active');
-                item.classList.add('left-menu__adaptive-sort-item_active')
+            }
+        })
+        function closeWindow(item){
+            item.querySelectorAll('.left-menu__sort-item-menu-point').forEach(point => {
+                point.addEventListener('click', () => {
+                    menuLeft.classList.remove('left-menu_sort-active');
+                    item.classList.remove('left-menu__adaptive-sort-item_active');
+                    item.querySelector('.left-menu__sort-item-info').innerHTML = point.textContent;
+                })
             })
+        }
+        this.itemsSortAdaptive.forEach(item => {
+            item.addEventListener('click', (e) => {
+                if(e.target.classList[0] !== 'left-menu__sort-item-menu-point'){
+                    menuLeft.classList.add('left-menu_sort-active');
+                    item.classList.add('left-menu__adaptive-sort-item_active');
+                }
+            });
+            closeWindow(item);
+        })
+    }
+    deletFilter(){
+        this.deletFilterBtn.addEventListener('click', () => {
+            document.querySelectorAll('.left-menu__sort-item-info').forEach(item => {
+                item.innerHTML = ''
+            });
+            this.menuLeft.querySelectorAll('input').forEach(input => {
+                input.checked = false;
+            });
         })
     }
 }
---
+++ src/js/modules/dropdown.js
@@ -0,0 +1,49 @@
+export default class Dropdown {
+    constructor(id){
+        this.drop = document.querySelector(id);
+        this.btn = this.drop.querySelector('.dropdown__btn');
+        this.click = this.drop.querySelector('.click-zone');
+        this.list = this.drop.querySelector('.dropdown__list');
+        this.items = this.drop.querySelectorAll('li');
+        this.dropThisItem = this.drop.querySelector('.dropdown__this-item');
+        this.startValue();
+        this.dropMain();
+        this.observer(this.btn, this.list);
+    }
+    startValue(){
+        this.dropThisItem.innerHTML = this.items[0].textContent
+    }
+    dropMain(){
+        document.addEventListener('click', (e) => {
+            console.log(e)
+            if(e.target.classList[0] !== this.click.classList[0]){
+                this.drop.classList.remove("dropdown_active");
+            }else{
+                this.drop.classList.toggle("dropdown_active");
+            }
+        })
+        this.items.forEach(item => {
+            item.addEventListener('click', () => {
+                this.dropThisItem.innerHTML = item.innerHTML
+            })
+        });
+    }
+    observer(btn, list){
+        const options = {
+            rootMargin: `0px 0px -${this.list.offsetHeight}px 0px`,
+            threshold: 1 
+        };  
+        const observer = new IntersectionObserver(callback, options);  
+        observer.observe(this.drop);
+
+        function callback(entries) {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    list.style.top = `${btn.offsetHeight}px`
+                } else {
+                    list.style.top = `-${list.offsetHeight}px`
+                }
+            });
+        }
+    }
+}
