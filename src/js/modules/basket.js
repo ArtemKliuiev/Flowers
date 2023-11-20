12864065798a1df5430f3c103533aa5527670bde
Denis Plishkan <denchik.plishkan@gmail.com>
2023-11-20 17:56:07 +0200

    work with basket

--- src/js/modules/basket.js
+++ src/js/modules/basket.js
@@ -3,156 +3,131 @@ async function basket() {
     const productCards = document.querySelectorAll('.product-card');
     const basketNumEl = document.querySelectorAll('.basket-amount p');
     const basketWrapper = document.querySelector('.basket__wrapper');
+    const basketWrapperRight = document.querySelector('.basket__wrapper-right');
+    const basketWrapperText = document.querySelector('.basket__wrapper-text');
     const basketMessage = document.querySelector('.basket__message');
     
     let basketNum = 0;
     let parsedBasket = [];
-    let quantNum = 1;
 
     function renderBasket() {
-      parsedBasket.forEach((item) => {
-        if (basketNum > 0) {
-          let template = `
-      <div class="basket__wrapper-left">
-                  <div class="basket__wrapper-text">
-                    <p>Название</p>
-                    <p>Цена за шт.</p>
-                    <p>Кол-во</p>
-                    <p>Итог</p>
-                  </div>
-            
-                  <div class="basket__wrapper-card basket__card">
-                    <div class="basket__card-img">
-                      <img src="${item.imgSrc}" />
-                    </div>
-                    <div class="basket__card-center-wrapper">
-                      <div class="basket__card-name">
-                        <p>${item.productName}</p>
-                      </div>
-                      <div class="basket__card-price">
-                        <p class="basket__card-new-price">${item.newPrice} ₴</p>
-                        <p class="basket__card-old-price">${item.oldPrice} ₴</p>
-                      </div>
-                      <div class="basket__card-num-wrapper">
-                        <div class="basket__card-arrow-left">
-                          <svg ><use xlink:href="./images/Sprite.svg#basket-arrow-left"></use></svg>
-                        </div>
-                        <div class="basket__card-num">
-                          <p>${item.quantNum}</p>
-                        </div>
-                        <div class="basket__card-arrow-right">
-                          <svg ><use xlink:href="./images/Sprite.svg#basket-arrow-right"></use></svg>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="basket__card-all-price">
-                      <p>${item.newPrice * basketNum} ₴</p>
-                    </div>
-                    <div class="basket__card-del-wrapper">
-                      <div class="basket__card-del">
-                        <svg ><use xlink:href="./images/Sprite.svg#basket-card-del"></use></svg>
-                      </div>
-                      <div class="product-card__top-like basket__like">
-                        <svg ><use xlink:href="./images/Sprite.svg#basket-like"></use></svg>
-                        <div class="product-card__top-like_active basket__like_active">
-                          <svg ><use xlink:href="./images/Sprite.svg#basket-like-active"></use></svg>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-            
-                <div class="basket__wrapper-right">
-                    <div class="basket__right-nav">
-                        <a href="#">Продолжить покупки</a>
-                        <svg ><use xlink:href="./images/Sprite.svg#basket-nav-arrow"></use></svg>
-                      </div>
-                      <div class="basket__wrapper-right-content">
-                        <h2 class="basket__wrapper-right-title">Ваш заказ</h2>
-                  <div class="basket__wrapper-right-inner">
-                    <div class="basket__wrapper-right-prices">
-                      <div class="basket__wrapper-right-products">
-                        <p>Товары</p>
-                      </div>
-                      <p class="basket__wrapper-right-price">5 558 ₴</p>
-                    </div>
-                    <div class="basket__wrapper-right-stock">
-                      <div class="basket__wrapper-right-products">
-                        <p>Скидка</p>
-                      </div>
-                      <p>254 ₴</p>
-                    </div>
-                  </div>
-                  <div class="basket__wrapper-right-all">
-                    <p class="basket__wrapper-right-all-text">Всего</p>
-                    <p>5 304 ₴</p>
-                  </div>
-                  <a href="./order.html">
-                    <div class="product-card__bottom-button-desktop order__button-wrapper">
-                      Оформить заказ
-                      <img
-                        src="./images/main/stock/button-branch.png"
-                        alt="button-branch"
-                      />
-                    </div>
-                  </a>
-                </div>
-                  
-                </div> 
-      `;
-    
-      if (basketWrapper) {
-        basketWrapper.insertAdjacentHTML('beforeend', template);
-    }
-    
-    const orderButton = document.querySelector('.product-card__bottom-button-desktop.order__button-wrapper');
-    if (orderButton) {
-    
-        orderButton.addEventListener('click', (e) => {
-            e.preventDefault();
-            sessionStorage.removeItem('basket');
-            window.location.href = './order.html';
-        });
-    }
-    } else {
-        if(basketMessage) {
-            basketMessage.style.display = 'block';
-        }   
-    }
-      })
-      
-    }
+      if (parsedBasket.length > 0) {
+        parsedBasket.forEach((item) => {
 
-    function quantProduct() {
-      const buttonWrapper = document.querySelector('.basket__wrapper');
-      const quantNumberEl = document.querySelector('.basket__card-num p');
+          renderAllPrice(item)
+
+          let template = `
+          <div class="basket__wrapper-left">   
+          <div class="basket__wrapper-card basket__card">
+            <div class="basket__card-img">
+              <img src="${item.imgSrc}" />
+            </div>
+            <div class="basket__card-center-wrapper">
+              <div class="basket__card-name">
+                <p>${item.productName}</p>
+              </div>
+              <div class="basket__card-price">
+                <p class="basket__card-new-price">${item.newPrice} ₴</p>
+                <p class="basket__card-old-price">${item.oldPrice} ₴</p>
+              </div>
+              <div class="basket__card-num-wrapper">
+                <div class="basket__card-arrow-left">
+                  <svg ><use xlink:href="./images/Sprite.svg#basket-arrow-left"></use></svg>
+                </div>
+                <div class="basket__card-num">
+                  <p>${item.quantNum}</p>
+                </div>
+                <div class="basket__card-arrow-right">
+                  <svg ><use xlink:href="./images/Sprite.svg#basket-arrow-right"></use></svg>
+                </div>
+              </div>
+            </div>
+            <div class="basket__card-all-price">
+              <p>${item.newPrice * item.quantNum} ₴</p>
+            </div>
+            <div class="basket__card-del-wrapper">
+              <div class="basket__card-del">
+                <svg ><use xlink:href="./images/Sprite.svg#basket-card-del"></use></svg>
+              </div>
+              <div class="product-card__top-like basket__like">
+                <svg ><use xlink:href="./images/Sprite.svg#basket-like"></use></svg>
+                <div class="product-card__top-like_active basket__like_active">
+                  <svg ><use xlink:href="./images/Sprite.svg#basket-like-active"></use></svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+          `;
     
-      if (buttonWrapper && quantNumberEl) {
-        buttonWrapper.addEventListener('click', (e) => {
-          const clickEl = e.target.parentNode;
-    
-          if (clickEl.classList.contains('basket__card-arrow-left')) {
-            quantNum -= 1;
-          } else if (clickEl.classList.contains('basket__card-arrow-right')) {
-            quantNum += 1;
+          if (basketWrapper) {
+            basketWrapper.insertAdjacentHTML('beforeend', template);
           }
     
-          quantNumberEl.textContent = quantNum;
-          renderAllPrice();
+          const orderButton = document.querySelector('.product-card__bottom-button-desktop.order__button-wrapper');
+          if (orderButton) {
+            orderButton.addEventListener('click', (e) => {
+              e.preventDefault();
+              sessionStorage.removeItem('basket');
+              window.location.href = './order.html';
+            });
+          }
+        });
+      } else {
+        if (parsedBasket && basketWrapperRight) {
+          basketWrapperRight.style.display = 'none';
+          basketWrapperText.style.display = 'none';
+          basketMessage.style.display = 'block';
+        }
+      }
+
+    }
+
+    
+    function quantProduct() {
+      const buttonWrapper = document.querySelector('.basket__wrapper');
+      if (buttonWrapper) {
+        buttonWrapper.addEventListener('click', (e) => {
+          const clickEl = e.target.parentNode;
+          const basketCard = clickEl.closest('.basket__wrapper-card');
+    
+          if (basketCard) {
+            const productId = basketCard.querySelector('.basket__card-img img').src;
+            const currentItem = parsedBasket.find((item) => item.imgSrc === productId);
+    
+            if (currentItem) {
+              if (clickEl.classList.contains('basket__card-arrow-left')) {
+                currentItem.quantNum = Math.max(1, currentItem.quantNum - 1);
+              } else if (clickEl.classList.contains('basket__card-arrow-right')) {
+                currentItem.quantNum += 1;
+              }
+    
+              const quantNumberEl = basketCard.querySelector('.basket__card-num p');
+    
+              if (quantNumberEl) {
+                quantNumberEl.textContent = currentItem.quantNum;
+                renderAllPrice(currentItem);
+              }
+    
+              updateLocalStorage();
+              sessionStorage.setItem('basket', JSON.stringify(parsedBasket));
+            }
+          }
         });
       }
     }
 
-    function renderAllPrice() {
-      const allPrice = document.querySelector('.basket__card-all-price p');
+    function renderAllPrice(item) {
+      const allPrice = document.querySelectorAll('.basket__wrapper-right-price');
+      console.log(allPrice);
     
-      if (allPrice) {
-        const currentItem = parsedBasket.find(item => item.productName === 'Название товара');
-    
-        if (currentItem) {
-          allPrice.textContent = currentItem.newPrice * quantNum + ' ' + '₴';
-        }
+      if (allPrice && item) {
+        allPrice.textContent = item.newPrice * item.quantNum + ' ₴';
       }
+    }
+
+    function updateLocalStorage() {
+      localStorage.setItem('basket', JSON.stringify(parsedBasket));
     }
     
     const storedBasket = sessionStorage.getItem('basket');
@@ -184,14 +159,19 @@ async function basket() {
               
             sessionStorage.setItem('basket', JSON.stringify(parsedBasket));
 
+            updateLocalStorage();
         });
     });
-    
+
     quantProduct();
     renderAllPrice();
     renderBasket();
     
+    
 }
 
 export default basket;
+
+
+
 
