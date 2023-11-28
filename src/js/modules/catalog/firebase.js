883e7cf4097b59421ad44c3d85c3fb03ffd128ba
artem <mr_artem>
2023-11-28 18:41:48 +0200

    add sort

--- src/js/modules/catalog/firebase.js
+++ src/js/modules/catalog/firebase.js
@@ -5,24 +5,47 @@ export default class loadFirebase{
     async load(info){
         const db = firebase.getFirestore();
         const colect = collection(db, "products");
+        let sort = ''
+        const filters = [
+            or(
+                where("bouquets", "in", info.bouquets),
+                where("roses", "in", info.roses),
+                where("in-box", "in", info.inBox),
+                where("compositions", "in", info.compositions),
+                where("gift", "in", info.gift),
+                where("gift-basket", "in", info.giftBasket),
+                where("for-bride", "in", info.forBride),
+                where("delicious", "in", info.delicious),
+            )
+        ];
+
+        if(info.whom.length != 0){
+            filters.push(where("whom", "==", info.whom))
+            console.log('whom')
+        }
+        if(info.color.length != 0){
+            filters.push(where("color", "==", info.color))
+            console.log('color')
+        }
+        if(info.occasion.length != 0){
+            filters.push(where("occasion", "==", info.occasion))
+            console.log('occasion')
+        }
+
+        if(info.sort === 'Цена(вверх)'){
+            sort = 'price'
+        }else if(info.sort === 'Цена(вниз)'){
+            sort = 'price', 'desc'
+        }else{
+            sort = 'price'
+        }
+
+        console.log(info.sort)
+
         const q = query(
             colect,
-            and(
-                or(
-                    where("whom", "in", info.whom),
-                    where("color", "in", info.color),
-                    where("occasion", "in", info.occasion),
-                    where("bouquets", "in", info.bouquets),
-                    where("roses", "in", info.roses),
-                    where("in-box", "in", info.inBox),
-                    where("compositions", "in", info.compositions),
-                    where("gift", "in", info.gift),
-                    where("gift-basket", "in", info.giftBasket),
-                    where("for-bride", "in", info.forBride),
-                    where("delicious", "in", info.delicious),
-                )
-            ),
-            orderBy('price')
+            and(...filters),
+            orderBy('price', 'desc')
         );
 
         const querySnapshot = await getDocs(q);
